13146555731ad9eab063df77e7b41981c8de370f
Franco Cabrera <cabrera.franco@outlook.com>
2023-03-23 17:07:20 -0300

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const routerApi = require('./routes');
+//const routerApi = require('./routes');
 const cors = require('cors');
-const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+//const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
@@ -39,3 +39,6 @@ app.get('/nueva-ruta', (req, res) => {
 app.listen(port, () => {
   console.log(`app running ${port}`);
 });
+
+
+module.exports = app
